9ee272017a4cf303b8c40c7a3be5bf3c4bad20c6
anderskambenders <a.zetsmanis@mail.ru>
2023-11-01 15:09:39 +0300

    feat: change errorBtn to function

--- src/components/error-boundary/ErrorBtn.tsx
+++ src/components/error-boundary/ErrorBtn.tsx
@@ -1,31 +1,18 @@
-import { Component } from 'react';
-import { Props } from '../types';
+import { useState } from 'react';
 
-type State = {
-  IsClicked: boolean;
+const ErrorBtn = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => setIsClicked(true);
+
+  if (isClicked) {
+    throw new Error('This is error');
+  }
+  return (
+    <button className="errBtn" onClick={handleClick}>
+      Error button
+    </button>
+  );
 };
 
-export default class ErrorBtn extends Component<Props, State> {
-  constructor(props: Props | Readonly<Props>) {
-    super(props);
-    this.state = {
-      IsClicked: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({ IsClicked: true });
-  }
-
-  render() {
-    if (this.state.IsClicked) {
-      throw new Error('This is error');
-    }
-    return (
-      <button className="errBtn" onClick={this.handleClick}>
-        Error button
-      </button>
-    );
-  }
-}
+export default ErrorBtn;
